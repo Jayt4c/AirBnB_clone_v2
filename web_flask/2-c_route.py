9f489de1a359ed01e636e553e55063d7892baae0
jayt4c <igejuwon53@gmail.com>
2024-05-17 08:34:53 -0700

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    starts a flask web app
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+        Displays "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        Displays "HBNB"
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_params(text):
+    """
+        Displays is_cool
+    """
+    text_no_underscore = text.replace('_', ' ')
+    return "C {}".format(text_no_underscore)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
