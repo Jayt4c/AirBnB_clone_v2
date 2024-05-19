d5c73d384fb4512b313df08eadf2c3ea85ace935
jayt4c <igejuwon53@gmail.com>
2024-05-19 10:06:11 -0700

    task 2 redo done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,6 +5,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """
@@ -12,12 +13,14 @@ def home():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
         Displays "HBNB"
     """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_with_params(text):
@@ -27,6 +30,6 @@ def c_with_params(text):
     text_no_underscore = text.replace('_', ' ')
     return "C {}".format(text_no_underscore)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
