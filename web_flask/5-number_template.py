eac4579bf4b74fe9da1863a7c8f713731d09eb3c
jayt4c <igejuwon53@gmail.com>
2024-05-19 10:12:03 -0700

    task 5 redo done

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,12 +5,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """
         Displays "Hello HBNB!"
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -19,6 +21,7 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_with_params(text):
     """
@@ -26,6 +29,7 @@ def c_with_params(text):
     """
     text_no_underscore = text.replace('_', ' ')
     return "C {}".format(text_no_underscore)
+
 
 @app.route('/python', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -36,12 +40,14 @@ def python_with_text_params(text):
     text_no_underscore = text.replace('_', ' ')
     return "python{}".format(text_no_underscore)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """
         display “n is a number” only if n is an integer
     """
     return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
@@ -50,6 +56,6 @@ def number_template(n):
     """
     return render_template('5-number.html', number=n)
 
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
