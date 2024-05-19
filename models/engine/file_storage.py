623b4ecd081e839396627975c0788fddaf41085c
jayt4c <igejuwon53@gmail.com>
2024-05-19 16:01:17 -0700

    task 7 redo

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -53,9 +53,10 @@ class FileStorage:
         try:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
-            for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+                for key in jo:
+                    self.__objects[key] =
+                    classes[jo[key]["__class__"]](**jo[key])
+        except (FileNotFoundError, json.JSONDecodeError):
             pass
 
     def delete(self, obj=None):
